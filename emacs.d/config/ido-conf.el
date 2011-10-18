df190c4e9656d95edfc2e9b7ffc975d179005eb6
Roy Deal Simon <roy.simon@gmail.com>
2011-10-18 14:36:13 +0200

    Emacs: ido ignore files

--- emacs.d/config/ido-conf.el
+++ emacs.d/config/ido-conf.el
@@ -1,7 +1,17 @@
+(require 'ido)
 (ido-mode t)
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
       ido-use-filename-at-point 'guess
-      ido-max-prospects 10)
-
+      ido-max-prospects 10
+      ido-ignore-buffers
+      '("\\` " "^\\*ESS\\*" "^\\*Messages\\*" "^\\*Help\\*" "^\\*Buffer"
+              "^\\*.*Completions\\*$" "^\\*Ediff" "^\\*tramp" "^\\*cvs-"
+              "_region_" " output\\*$" "^TAGS$" "^\*Ido" "^archive-contents" "^\\*eshell\\*"
+              "^\\*grep\\*")
+      ido-ignore-directories
+      '("\\`auto/" "\\.prv/" "\\`CVS/" "\\`\\.\\./" "\\`\\./")
+      ido-ignore-files
+      '("\\`auto/" "\\.prv/" "_region_" "\\`CVS/" "\\`#" "\\`.#"
+        "\\`\\.\\./" "\\`\\./"))
