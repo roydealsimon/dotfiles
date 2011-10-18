002f444826d635b9caa7e7107540edd3ad213d7c
Roy Deal Simon <roy.simon@gmail.com>
2011-10-18 20:03:09 +0200

    Emacs: add multi-term-conf

--- emacs.d/config/multi-term-conf.el
+++ emacs.d/config/multi-term-conf.el
@@ -1,2 +1,12 @@
-(require 'multi-term)
+;; (require 'multi-term)
+(autoload 'multi-term "multi-term" nil t)
+(autoload 'multi-term-next "multi-term" nil t)
+
 (setq multi-term-program "/bin/bash")
+
+;; only needed if you use autopair
+(add-hook 'term-mode-hook
+  #'(lambda () (setq autopair-dont-activate t)))
+
+(global-set-key (kbd "C-c t") 'multi-term-next) ;; navigate through the terms
+(global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
