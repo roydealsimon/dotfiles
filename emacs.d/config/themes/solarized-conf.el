1de5b7dbeae0255203e7d22a21ea4c022d0acecc
Roy Deal Simon <roy.simon@gmail.com>
2011-10-18 14:36:56 +0200

    Emacs: clearer background color for solarized light theme

--- emacs.d/config/themes/solarized-conf.el
+++ emacs.d/config/themes/solarized-conf.el
@@ -1,3 +1,6 @@
 (add-themes-path "emacs-color-theme-solarized")
 (load-library "color-theme-solarized")
 (color-theme-solarized-light)
+
+(set-face-background 'region "#93a1a1")
+(global-set-key (kbd "C-c l") 'color-theme-solarized)
